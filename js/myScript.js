5c588bd5a02d102fcff356a27e925a608a7600aa
Vincent Ygbuhay <ygbuhay.vincent@yahoo.com>
2025-04-08 10:30:14 +0800

    Initial commit

---
+++ js/myScript.js
@@ -0,0 +1,7 @@
+function myColors() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const color = `rgb(${r}, ${g}, ${b})`;
+    document.getElementById("demo").style.backgroundColor = color;
+}
